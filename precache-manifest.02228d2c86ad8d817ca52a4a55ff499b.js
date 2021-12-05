015e19e18708eff9bb824a3b57f8125f2a9a0def
charlyautomatiza <charlyautomatiza@users.noreply.github.com>
2021-12-05 18:52:48 +0000

    deploy: 361ea169bd9e996c35e682c6752042597218c8af

---
+++ precache-manifest.02228d2c86ad8d817ca52a4a55ff499b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbfd0e63d02d6ab52ed67930458588c",
+    "url": "https://charlyautomatiza.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "ead2d1823fc7e6758432",
+    "url": "https://charlyautomatiza.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "9d759c3337be6f814f1d",
+    "url": "https://charlyautomatiza.github.io/task-management-frontend/static/js/2.a621b5d4.chunk.js"
+  },
+  {
+    "revision": "ead2d1823fc7e6758432",
+    "url": "https://charlyautomatiza.github.io/task-management-frontend/static/js/main.bc9ef8ae.chunk.js"
+  },
+  {
+    "revision": "537ea284ae9af6a1fbb8",
+    "url": "https://charlyautomatiza.github.io/task-management-frontend/static/js/runtime~main.37b5816e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://charlyautomatiza.github.io/task-management-frontend/precache-manifest.02228d2c86ad8d817ca52a4a55ff499b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://charlyautomatiza.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
